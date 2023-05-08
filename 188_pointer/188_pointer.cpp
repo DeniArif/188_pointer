4d15e46f050186f704c8c5f55c42c265b2eca634
DeniArif <deniarifwibowo82@gmail.com>
2023-05-08 15:01:29 +0700

    comit

--- 188_pointer/188_pointer.cpp
+++ 188_pointer/188_pointer.cpp
@@ -16,5 +16,7 @@ int main()
 	mahasiswa mhs{ 1 };		//object mhs
 	mhs.ShowNim();			//Member Acces Operator
 
-	mahasiswa& ref = mhs;
+	mahasiswa& ref = mhs;	//Pointer Reference refMhs
+	ref.nim = 2;			//Member Operator
+	
 }
