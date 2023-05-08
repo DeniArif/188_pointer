44220a1b65a3fc242f32d6d9e0f7cc158bb9c7bd
DeniArif <deniarifwibowo82@gmail.com>
2023-05-08 14:54:27 +0700

    comit

--- 188_pointer/188_pointer.cpp
+++ 188_pointer/188_pointer.cpp
@@ -6,3 +6,12 @@ public:
 	int nim;
 	void ShowNim(); //deklarasi method
 };
+
+void mahasiswa::ShowNim() {//implementasi method diluar class
+	cout << "No Induk = " << nim << endl;
+}
+
+int main()
+{
+	maha
+}
